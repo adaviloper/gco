f2c5f8dc5c284f62f69ad5333028458c2aac61b7
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-10-23 21:39:37 -0500

    initial bug checkout logic

--- cmd/bug.go
+++ cmd/bug.go
@@ -1,12 +1,10 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/adaviloper/gco/git"
 	"github.com/spf13/cobra"
 )
 
@@ -21,7 +19,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("bug called")
+		git.CheckoutBranch("test")
 	},
 }
 
