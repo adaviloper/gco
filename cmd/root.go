14018c0c49178a4dd561286756cfa5b36512b1d3
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-10-23 23:48:17 -0500

    adding config read for ticket type prefixes

--- cmd/root.go
+++ cmd/root.go
@@ -4,13 +4,19 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"errors"
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/adaviloper/gco/internal/branch"
 	"github.com/adaviloper/gco/internal/bug"
 	"github.com/adaviloper/gco/internal/story"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -20,6 +26,9 @@ var rootCmd = &cobra.Command{
 	gco full/branch-name
 	gco <some-substring>
 	gco <ticket_number>`,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		return initializeConfig(cmd)
+	},
 	// Args: cobra.NoArgs(),
 	// Uncoment the following line if your bare application
 	// has an action associated with it:
@@ -63,4 +72,50 @@ func init() {
 	rootCmd.Flags().BoolP("story", "s", false, "Create a story ticket")
 }
 
+func initializeConfig(cmd *cobra.Command) error {
+	// 1. Set up Viper to use environment variables.
+	viper.SetEnvPrefix("MYAPP")
+	// Allow for nested keys in environment variables (e.g. `MYAPP_DATABASE_HOST`)
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "*", "-", "*"))
+	viper.AutomaticEnv()
 
+	// 2. Handle the configuration file.
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Search for a config file in default locations.
+		home, err := os.UserHomeDir()
+		// Only panic if we can't get the home directory.
+		cobra.CheckErr(err)
+
+		// Search for a config file with the name "config" (without extension).
+		viper.AddConfigPath(".")
+		viper.AddConfigPath(home + "/.config/gco")
+		viper.SetConfigName("config")
+		viper.SetConfigType("yaml")
+	}
+
+	// 3. Read the configuration file.
+	// If a config file is found, read it in. We use a robust error check
+	// to ignore "file not found" errors, but panic on any other error.
+	if err := viper.ReadInConfig(); err != nil {
+		// It's okay if the config file doesn't exist.
+		var configFileNotFoundError viper.ConfigFileNotFoundError
+		if !errors.As(err, &configFileNotFoundError) {
+			return err
+		}
+	}
+
+	// 4. Bind Cobra flags to Viper.
+	// This is the magic that makes the flag values available through Viper.
+	// It binds the full flag set of the command passed in.
+	err := viper.BindPFlags(cmd.Flags())
+	if err != nil {
+		return err
+	}
+
+	// This is an optional but useful step to debug your config.
+	fmt.Println("Configuration initialized. Using config file:", viper.ConfigFileUsed())
+	return nil
+}
--- internal/bug/bug.go
+++ internal/bug/bug.go
@@ -10,11 +10,25 @@ import (
 	"github.com/adaviloper/gco/internal/git"
 	"github.com/adaviloper/gco/internal/str_utils"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func Run(cmd *cobra.Command, args []string) {
-	fmt.Printf("args: %v\n", args)
+	repos := viper.Get("repositories.gco")
+	var prefix string
+	if list, ok := repos.([]interface{}); ok {
+		for _, item := range list {
+			if m, ok := item.(map[string]interface{}); ok {
+				if val, ok := m["bug"]; ok {
+					prefix = fmt.Sprintf("%v", val)
+					break
+				}
+			}
+		}
+	}
+
+	fmt.Printf("Prefix: %s\n", prefix)
 	branch := str_utils.Slugify(strings.Join(args, "-"))
-	git.CheckoutBranch(fmt.Sprintf("bugfix/%s", branch))
+	git.CheckoutBranch(fmt.Sprintf("%s/%s", prefix, branch))
 }
 
